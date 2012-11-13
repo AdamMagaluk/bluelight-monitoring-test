16cec744a8449eda310052f591b3dbe6ade5d80e
Adam Magaluk <adam@adamm-debian.illuminatingconcepts.local>
2012-11-13 11:19:02 -0500

    Changed to POST

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var rejectMsg = '<?xml version="1.0" encoding="UTF-8"?><Response><Reject /></Res
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.post('/', function(request, response) {
   response.send(rejectMsg);
 });
 
